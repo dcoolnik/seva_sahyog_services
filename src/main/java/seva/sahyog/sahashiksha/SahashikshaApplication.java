0c2c4a4b3597fba40a0658ab19a3a2c2fdec4ab2
Nikhil Dafale <dafale.nikhil@gmail.com>
2020-07-19 20:40:22 +0530

    Initial commit

---
+++ src/main/java/seva/sahyog/sahashiksha/SahashikshaApplication.java
@@ -0,0 +1,13 @@
+package seva.sahyog.sahashiksha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahashikshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahashikshaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seva/sahyog/sahashiksha/SahashikshaApplicationTests.java
@@ -0,0 +1,13 @@
+package seva.sahyog.sahashiksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahashikshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
