5b97c30254f51fd3a71ea9bf1c9a76c2683ad042
prince1340 <aec.prince@gmail.com>
2020-10-18 14:50:22 +0000

    Prince : Added configuration for fetching properties per environment basis.

--- src/main/java/seva/sahyog/sahashiksha/application/SahashikshaApplication.java
+++ src/main/java/seva/sahyog/sahashiksha/application/SahashikshaApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "seva.sahyog.sahashiksha")
 @EnableJpaRepositories("seva.sahyog.sahashiksha.dao")
 @EntityScan("seva.sahyog.sahashiksha")
+@PropertySource("application-${spring.profiles.active}.properties")
 public class SahashikshaApplication {
 
 	public static void main(String[] args) {
