6b8881f3f63d935b3a64b57a72f56a6cd250f809
prince1340 <aec.prince@gmail.com>
2020-10-27 09:41:45 +0000

    Added Swagger related changes.

--- src/main/java/seva/sahyog/sahashiksha/application/SahashikshaApplication.java
+++ src/main/java/seva/sahyog/sahashiksha/application/SahashikshaApplication.java
@@ -3,19 +3,31 @@ package seva.sahyog.sahashiksha.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "seva.sahyog.sahashiksha")
 @EnableJpaRepositories("seva.sahyog.sahashiksha.dao")
 @EntityScan("seva.sahyog.sahashiksha")
 @PropertySource("application-${spring.profiles.active}.properties")
+@EnableSwagger2
 public class SahashikshaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SahashikshaApplication.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("seva.sahyog.sahashiksha")).build();
 	}
 
 }
